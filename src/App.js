b6c825f86068a42614bdcd3387da3e0390c490e2
Joao Pedro <joaopedro.santosoliveira2003@gmail.com>
2022-09-18 13:18:28 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <h1>João Pedro</h1>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      nome: "João Pedro"
+    }
+
+  }
+  render(){
+    const {nome} = this.state
+    return(
+      <div>
+        <h1>Olá, meu nome é {nome}</h1>
+      </div>
+      
+    )
+  }
 }
+
 
 export default App;
